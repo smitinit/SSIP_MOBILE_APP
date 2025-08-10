5c660473dc42a135f175604cb56615b59c3ddd71
smitinit <patelsmit090305@gmail.com>
2025-08-10 17:00:54 +0530

    ..

--- Android_Application/src/screens/ChatBotScreen.tsx
+++ Android_Application/src/screens/ChatBotScreen.tsx
@@ -418,7 +418,7 @@ export default function ChatBotScreen() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={Platform.select({
         ios: insets.top + 8,
-        android: insets.bottom + 10,
+        android: insets.bottom + 30,
         default: 0,
       })}
     >
@@ -565,7 +565,8 @@ const styles = StyleSheet.create({
   composerInner: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "",
+
     borderRadius: 28,
     borderWidth: 1,
     borderColor: "#E2E8F0",
