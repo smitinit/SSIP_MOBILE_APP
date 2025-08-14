2407cb1872ec901c328a59f9aa6ffee685cf1031
smitinit <patelsmit090305@gmail.com>
2025-08-14 22:48:44 +0530

    clean package js and home

--- Android_Application/app/_layout.tsx
+++ Android_Application/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { useFonts } from "expo-font";
 import { ClerkProvider } from "@clerk/clerk-expo";
-import { router, Slot } from "expo-router";
+import { Slot } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, useColorScheme } from "react-native";
 import { palette } from "@/design/tokens";
